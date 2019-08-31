8b454f645d12653b8cdf454021394078529d810e
Utsav Patel <utsavpatel5@gmail.com>
2019-08-31 16:13:42 -0400

    Changes to Dolly!

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -4,6 +4,7 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello Dolly from Eclipse");
 		System.out.println("Hello Dolly from Eclipse Again");
 	}
 
